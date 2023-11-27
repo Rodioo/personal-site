2d1dcd4d2b3d149a5115e9dc25e9707282346e1a
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2023-11-27 09:20:56 +0200

    started working on Home page
    SUMMARY:
      • Started implementing the mobile layout
      • Added home photo and adjusted it's size for small screens
      • Added the main text info for the hero section and the basic buttons (need to style them too)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import homePhoto from '../assets/home_photo.png';
+
 function Home() {
   return (
-    <div data-testid="HOME">
-      <span className='text-white'>Home</span>
+    <div
+      data-testid="HOME"
+      className="mt-24 flex h-screen w-screen flex-col p-6">
+      <div className="flex flex-col font-lato">
+        <span className="text-5xl tracking-wider text-white">Hi,</span>
+        <span className="mt-2 text-5xl tracking-wider text-white">
+          I'm Antonio Fălcescu
+        </span>
+        <span className="mt-4 text-3xl font-medium tracking-widest text-taupe-gray">
+          Software Developer
+        </span>
+        <div className="mt-12 flex w-2/5 justify-between">
+          <button className="text-white">Learn More</button>
+          <button className="text-white">Resume</button>
+        </div>
+      </div>
+      <img
+        src={homePhoto}
+        alt=""
+        className="mt-24 w-fit text-center"
+      />
     </div>
   );
 }
