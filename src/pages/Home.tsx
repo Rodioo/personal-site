4b98d5c04c28ba8a04e1db6a2dc382498fe4dd26
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2023-12-07 13:00:17 +0200

    adjusted responsiveness for multiple sizes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
 import homePhoto from '../assets/home_photo.png';
 
 //TODO: add button for primary CTA and useNavigate for redirect
-// TODO: think how to scale image width fro md: screen size
 function Home() {
   return (
     <div
       data-testid="HOME"
-      className="m-8 mt-24 flex flex-col border lg:flex-row lg:justify-between">
-      <div className="flex flex-col font-lato">
-        <span className="text-5xl tracking-wider text-white">Hi,</span>
-        <span className="mt-2 text-5xl tracking-wider text-white">
+      className="mx-8 mt-20 flex flex-col md:mx-32 lg:mx-12 lg:flex-row lg:justify-between xl:mx-32 2xl:mx-40">
+      <div className="mb-auto mt-auto flex flex-col font-lato">
+        <span className="text-5xl tracking-wider text-white 2xl:text-6xl">
+          Hi,
+        </span>
+        <span className="mt-2 text-5xl tracking-wider text-white 2xl:text-6xl">
           I'm Antonio Fălcescu
         </span>
-        <span className="mt-4 text-3xl font-medium tracking-widest text-taupe-gray">
+        <span className="mt-4 text-3xl font-medium tracking-widest text-taupe-gray 2xl:text-4xl">
           Software Developer
         </span>
         <div className="mt-12">
@@ -23,7 +24,7 @@ function Home() {
       <img
         src={homePhoto}
         alt=""
-        className="mt-24 w-full border text-center md:border-red-500 lg:mt-0 lg:w-96 lg:border-green-500 xl:border-blue-400"
+        className="mt-24 w-full text-center lg:mt-0 lg:w-2/5"
       />
     </div>
   );
