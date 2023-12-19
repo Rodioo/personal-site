d28d38d3d216160d5d1658fa3847b2c8ed33cf5a
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2023-12-19 19:57:15 +0200

    added project card to work section

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type Props = {
+  className?: string;
+  backgroundSrc: string;
+  title: string;
+  description: string;
+};
+
+const ProjectCard = ({
+  className = '',
+  backgroundSrc,
+  title,
+  description,
+}: Props): React.JSX.Element => {
+  return (
+    <div className={`flex flex-col gap-4 ${className}`}>
+      <img
+        src={backgroundSrc}
+        alt={undefined}
+        className="cursor-pointer h-24 grayscale
+        duration-700 ease-in-out hover:-translate-y-0.5 hover:scale-105 hover:grayscale-0"
+      />
+      <div>
+        <p className="text-lg tracking-widest">{title}</p>
+        <p className="text-justify text-lg font-light tracking-wide">
+          &emsp;{description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,35 +3,65 @@ import {FaGithub, FaLinkedin} from 'react-icons/fa';
 import {SiLeetcode} from 'react-icons/si';
 import Social from '../../components/Social/Social.tsx';
 import HeaderParagraph from '../../components/HeaderParagraph/HeaderParagraph.tsx';
-import Resume from '../../components/Resume/Resume.tsx';
+import {FaDownload} from 'react-icons/fa';
+import Button from '../../components/Button/Button.tsx';
+import ButtonType from '../../common/types/button.type.ts';
+import ProjectCard from '../../components/ProjectCard/ProjectCard.tsx';
+import pic from '../../assets/pic.jpeg';
+import { FaRegFolderOpen } from "react-icons/fa6";
 
 const About = (): React.JSX.Element => {
   return (
     <div
       data-testid="ABOUT"
-      className="mx-6 mt-20 flex flex-col gap-16 font-lato">
-      <HeaderParagraph title={'About me'}>
-        <p className="text-justify text-lg font-light tracking-wide">
-          &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </HeaderParagraph>
-      <HeaderParagraph title={'Work'}>
-        <p className="text-justify text-lg font-light tracking-wide">
-          &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </HeaderParagraph>
+      className="mx-6 mt-8 flex flex-col gap-8 font-lato">
+      <div>
+        <HeaderParagraph title={'About me'}>
+          <p className="text-justify text-lg font-light tracking-wide">
+            &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </HeaderParagraph>
+        <Button
+          className="ml-auto mr-auto mt-4"
+          type={ButtonType.Primary}
+          text={'Resume'}
+          icon={<FaDownload className="m-auto h-4 w-4 lg:h-4 lg:w-4" />}
+          onClick={() => {}}
+        />
+      </div>
+      <div>
+        <HeaderParagraph title={'Work'}>
+          <ProjectCard
+            className="mt-2"
+            backgroundSrc={pic}
+            title={'Lorem Ipsum'}
+            description={
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+            }
+          />
+          <ProjectCard
+            className="mt-2"
+            backgroundSrc={pic}
+            title={'Lorem Ipsum'}
+            description={
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+            }
+          />
+        </HeaderParagraph>
+        <Button
+          className="ml-auto mr-auto mt-4"
+          type={ButtonType.Primary}
+          text={'All Projects'}
+          icon={<FaRegFolderOpen className="m-auto h-4 w-4 lg:h-4 lg:w-4" />}
+          onClick={() => {}}
+        />
+      </div>
       <HeaderParagraph title={'Socials'}>
         <Social
           icon={<FaGithub className="h-6 w-6" />}
@@ -49,7 +79,6 @@ const About = (): React.JSX.Element => {
           text="Rodioo"
         />
       </HeaderParagraph>
-      {/*<Resume />*/}
     </div>
   );
 };
