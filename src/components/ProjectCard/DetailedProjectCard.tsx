5495a36095876d288f3552a8aa30f3c0fc0cc2dd
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2024-01-12 09:08:22 +0200

    refactored DetailedProjectCard component

--- src/components/ProjectCard/DetailedProjectCard.tsx
+++ src/components/ProjectCard/DetailedProjectCard.tsx
@@ -6,8 +6,9 @@ import ProjectInfo from '../../common/types/project/projectInfo.type.ts';
 import {AiOutlineDeploymentUnit} from 'react-icons/ai';
 import {FaCode} from 'react-icons/fa6';
 import {FaGitAlt, FaRegCalendar, FaRegCalendarPlus} from 'react-icons/fa';
-import { MdOutlineRocketLaunch } from "react-icons/md";
+import {MdOutlineRocketLaunch} from 'react-icons/md';
 import Social from '../Social/Social.tsx';
+import ProjectInfoCard from './ProjectInfoCard.tsx';
 
 type Props = {
   className?: string;
@@ -15,6 +16,7 @@ type Props = {
 };
 
 const images = [home, discover, genre];
+const PROJECT_INFO_CARD_ICON_STYLE = 'ml-0 h-8 w-8 text-baby-blue';
 
 const DetailedProjectCard = ({
   className = '',
@@ -35,77 +37,76 @@ const DetailedProjectCard = ({
           ))}
         </div>
       </div>
-      <div className="grid grid-cols-2 grid-rows-3 gap-x-6 gap-y-12 text-center">
+      <div className="grid grid-cols-2 grid-rows-3 gap-x-6 gap-y-12">
         {/*<p className="text-xl tracking-widest">More details</p>*/}
-        <div className="flex flex-col gap-2 text-center">
-          <AiOutlineDeploymentUnit className="h-8 w-8 text-baby-blue ml-auto mr-auto" />
-          <p className="text-2xl tracking-widest">Platform</p>
+        <ProjectInfoCard
+          icon={
+            <AiOutlineDeploymentUnit className={PROJECT_INFO_CARD_ICON_STYLE} />
+          }
+          title="Platform">
           <p>
-            This project was built for {projectInfo.platform === 'Web' && 'the '}
+            This project was built for{' '}
+            {projectInfo.platform === 'Web' && 'the '}
             {projectInfo.platform}
           </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <FaCode className="h-8 w-8 text-baby-blue ml-auto mr-auto" />
-          <p className="text-2xl tracking-widest">Tech stack</p>
+        </ProjectInfoCard>
+        <ProjectInfoCard
+          icon={<FaCode className={PROJECT_INFO_CARD_ICON_STYLE} />}
+          title="Tech stack">
           <p>{projectInfo.stack.replace(/;;/g, ', ')}</p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <FaRegCalendar className="h-8 w-8 text-baby-blue ml-auto mr-auto" />
-          <p className="text-2xl tracking-widest">Created on</p>
+        </ProjectInfoCard>
+        <ProjectInfoCard
+          icon={
+            <FaRegCalendar className={PROJECT_INFO_CARD_ICON_STYLE} />
+          }
+          title="Created on">
           <p>
             {projectInfo.createdAt
               ? projectInfo.createdAt.toLocaleDateString()
               : 'N/A'}
           </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <FaRegCalendarPlus className="h-8 w-8 text-baby-blue ml-auto mr-auto" />
-          <p className="text-2xl tracking-widest">Last updated on</p>
+        </ProjectInfoCard>
+        <ProjectInfoCard
+          icon={
+            <FaRegCalendarPlus className={PROJECT_INFO_CARD_ICON_STYLE} />
+          }
+          title="Last updated on">
           <p>
             {projectInfo.updatedAt
               ? projectInfo.updatedAt.toLocaleDateString()
               : 'N/A'}
           </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <FaGitAlt className="h-8 w-8 text-baby-blue ml-auto mr-auto" />
-          <p className="text-2xl tracking-widest">Source code</p>
-          {projectInfo.gitLink && projectInfo.gitPlatform
-            ? (
-              <Social
-                className='ml-auto mr-auto'
-                icon={projectInfo.gitPlatform.icon}
-                link={projectInfo.gitLink}
-                text={projectInfo.gitPlatform.label}
-              />
-            )
-            : (
-              <p className='p-2'>
-                N/A
-              </p>
-            )
+        </ProjectInfoCard>
+        <ProjectInfoCard
+          icon={<FaGitAlt className={PROJECT_INFO_CARD_ICON_STYLE} />}
+          title="Source code">
+          {projectInfo.gitLink && projectInfo.gitPlatform ? (
+            <Social
+              className="ml-0"
+              icon={projectInfo.gitPlatform.icon}
+              link={projectInfo.gitLink}
+              text={projectInfo.gitPlatform.label}
+            />
+          ) : (
+            <p className="p-2">N/A</p>
+          )}
+        </ProjectInfoCard>
+        <ProjectInfoCard
+          icon={
+            <MdOutlineRocketLaunch className={PROJECT_INFO_CARD_ICON_STYLE} />
           }
-        </div>
-        <div className="flex flex-col gap-2">
-          <MdOutlineRocketLaunch className="h-8 w-8 text-baby-blue ml-auto mr-auto" />
-          <p className="text-2xl tracking-widest">Published on</p>
-          {projectInfo.appLink && projectInfo.publishedPlatform
-            ? (
-                <Social
-                  className='ml-auto mr-auto'
-                  icon={projectInfo.publishedPlatform.icon}
-                  link={projectInfo.appLink}
-                  text={projectInfo.publishedPlatform.label}
-                />
-              )
-            : (
-              <p className='p-2'>
-                N/A
-              </p>
-            )
-          }
-        </div>
+          title="Published on">
+          {projectInfo.appLink && projectInfo.publishedPlatform ? (
+            <Social
+              className="ml-0"
+              icon={projectInfo.publishedPlatform.icon}
+              link={projectInfo.appLink}
+              text={projectInfo.publishedPlatform.label}
+            />
+          ) : (
+            <p className="p-2">N/A</p>
+          )}
+        </ProjectInfoCard>
       </div>
     </div>
   );
